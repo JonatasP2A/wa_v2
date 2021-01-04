eed872bef6426363ee04543b218d603a51409e01
Jonatas Alves <jonatasalves@id.uff.br>
2021-01-04 09:02:22 -0300

    endpoint and controller of profile

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+class ShowProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRespository: IUsersRepository,
+  ) {}
+
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const user = await this.usersRespository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
